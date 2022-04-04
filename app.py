28df3cbd333eb578cc1d0edcca3af13f1bd985f0
JanderRaul <janderr4ul@gmail.com>
2022-04-03 22:37:52 -0300

    firebase

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, RadioField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db as dbFire
+
+cred = credentials.Certificate("firebase-sdk.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://appcaracoroa-default-rtdb.firebaseio.com/'
+})
+
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -18,7 +27,8 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    # return User.query.get(int(user_id))
+    return dbFire.reference("User").get()[user_id]
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,7 +42,12 @@ class RegisterForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Digite sua senha"})
     submit = SubmitField("Registrar")
     def validate_username(self, username):
-        existing_user_name = User.query.filter_by(username=username.data).first()
+        # existing_user_name = User.query.filter_by(username=username.data).first()
+        existing_user_name = True
+        try:
+            ref = dbFire.reference("User").get()[username.data]
+        except:
+            existing_user_name = False
         if existing_user_name:
             raise ValidationError("Este nome já está sendo utilizado. Escolha outro nome!!")
 
@@ -54,77 +69,89 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
-
+        user = dbFire.reference('User').get()[form.username.data]
+        if user['senha'] == form.password.data:
+            global usuario_atual
+            usuario_atual = user['name']
+            return redirect(url_for('dashboard'))
+    
     return render_template('login.html', form=form)
 
 @app.route('/logout', methods=['GET', 'POST'])
-@login_required
 def logout():
-    logout_user()
+    global usuario_atual
+    usuario_atual = None
     return redirect(url_for('login'))
 
 @app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
 def dashboard():
-    user = User.query.get(current_user.id)
+    user = usuario_atual
     form = JogarForm()
     resultado = ''
     moeda = url_for('static', filename='inicio.png')
 
     result = random.randint(0, 1)
+    ref = dbFire.reference("User")
+    emp_ref = ref.get()[usuario_atual]
 
     if form.validate_on_submit():
         if result == 0:
             moeda = url_for('static', filename='moeda_cara.png')
         else:
             moeda = url_for('static', filename='moeda_coroa.png')
-        if int(form.jogar.data) == result:
-            newNum = user.nrJogos + 1
-            newVit = user.ganhos + 1
-            db.session.query(User).filter(User.id == user.id).update({User.nrJogos: newNum, User.ganhos: newVit})
-            db.session.commit()
+        if int(form.jogar.data) == result:            
+            newNum = int(emp_ref['jogos']) + 1
+            newVit = int(emp_ref['vitorias']) + 1
+            ref.child(usuario_atual).update({
+                'jogos': f'{newNum}',
+                'vitorias': f'{newVit}'
+            })
             resultado = 'Você ganhou'
         else:
-            newNum = user.nrJogos + 1
-            db.session.query(User).filter(User.id == user.id).update({User.nrJogos: newNum})
-            db.session.commit()
+            newNum = int(emp_ref['jogos']) + 1
+            ref.child(usuario_atual).update({
+                'jogos': f'{newNum}',
+            })
             resultado = 'Você perdeu'
 
-    return render_template('dashboard.html', nome=str(user.username), form=form, resultado=resultado, moeda=moeda)
+    return render_template('dashboard.html', nome=str(user), form=form, resultado=resultado, moeda=moeda)
 
+
+def criarUsuario(usuario):
+    ref = dbFire.reference('User')    
+    ref.update({
+        f'{usuario.username}':
+        {
+            'name': f'{usuario.username}',
+            'senha': f'{usuario.password}',
+            'jogos': usuario.nrJogos,
+            'vitorias': usuario.ganhos
+        }
+    })
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password, nrJogos=0, ganhos=0)
-        db.session.add(new_user)
-        db.session.commit()
+        new_user = User(username=form.username.data, password=form.password.data, nrJogos=0, ganhos=0)
+        criarUsuario(new_user)
         return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
 
 @app.route('/resultados', methods=['GET', 'POST'])
-@login_required
 def resultados():
-    user = User.query.get(current_user.id)
+    user = dbFire.reference("User").get()[usuario_atual]
     names = ['Vitorias', 'Derrotas']
-    vitorias = user.ganhos
-    derrotas = user.nrJogos - user.ganhos
+    vitorias = int(user['vitorias'])
+    derrotas = int(user['jogos']) - int(user['vitorias'])
     values = [vitorias, derrotas]
     return render_template('resultados.html', names=names, values=values)
 
 @app.route('/ranking', methods=['GET', 'POST'])
-@login_required
 def ranking():
-    users = User.query.all()
+    users = dbFire.reference("User").get()
     return render_template('ranking.html', users=users)
 
 if __name__ == '__main__':
